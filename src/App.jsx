d8985a1d6b3032de91e7d9040a73392ff82243ef
KramYengir <kramyengir@gmail.com>
2024-02-12 15:41:06 +0000

    add basic router structure to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
-import Design from "./components/Design";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Home from "./pages/Home/Home";
 
-const App = () => {
-  return <Design />;
-};
+function App() {
+  return (
+    <Router>
+      <div>
+        <Header />
+        <Routes>
+          <Route path="/" exact component={Home} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
 
 export default App;
