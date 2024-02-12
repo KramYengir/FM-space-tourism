36126a568dbfed175fb5d5184f5d6e44fa4c8bdb
KramYengir <kramyengir@gmail.com>
2024-02-12 18:45:36 +0000

    use Outlet & Layout for routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,35 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Outlet,
+} from "react-router-dom";
 import Header from "./components/Header/Header";
 import Home from "./pages/Home/Home";
+import Crew from "./pages/Crew/Crew";
+import Technology from "./pages/Technology/Technology";
+import Destination from "./pages/Destination/Destination";
+
+const Layout = () => {
+  return (
+    <div>
+      <Header />
+      <Outlet />
+    </div>
+  );
+};
 
 function App() {
   return (
     <Router>
-      <div>
-        <Header />
-        <Routes>
-          <Route path="/" exact component={Home} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/destination" element={<Destination />} />
+          <Route path="/crew" element={<Crew />} />
+          <Route path="/technology" element={<Technology />} />
+        </Route>
+      </Routes>
     </Router>
   );
 }
